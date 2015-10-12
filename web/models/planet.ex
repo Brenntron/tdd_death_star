f4bf2f58efc9b722e0cb91528804b6281dfb2ee4
Brennan Willingham <brennanw22@gmail.com>
2015-10-11 21:40:34 -0500

    Added planet model.

---
+++ web/models/planet.ex
@@ -0,0 +1,31 @@
+defmodule TddDeathStar.Planet do
+  use TddDeathStar.Web, :model
+
+  schema "planets" do
+    field :x, :integer
+    field :y, :integer
+    field :z, :integer
+    field :description, :string
+    field :name, :string
+    field :destroyed, :boolean, default: false
+
+    timestamps
+  end
+
+  @required_fields ~w(x y z description name)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+    |> validate_number(:x, greater_than: 0, less_than: 1000)
+    |> validate_number(:y, greater_than: 0, less_than: 1000)
+    |> validate_number(:z, greater_than: 0, less_than: 1000)
+  end
+end
